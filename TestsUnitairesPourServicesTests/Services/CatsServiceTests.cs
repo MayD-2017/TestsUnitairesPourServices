19495c487efc76900f7ed4578187aab1ec2dea06
Mishka <158780709+MayD-2017@users.noreply.github.com>
2025-04-02 13:03:10 -0400

    FCT : Suposément terminé.
    Veut pas exécuter???? Je vais voir la solution.

--- TestsUnitairesPourServicesTests/Services/CatsServiceTests.cs
+++ TestsUnitairesPourServicesTests/Services/CatsServiceTests.cs
@@ -8,46 +8,48 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TestsUnitairesPourServices.Data;
 using TestsUnitairesPourServices.Models;
+using Microsoft.Extensions.Options;
+using TestsUnitairesPourServices.Exceptions;
+using System.Security.Cryptography;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 
 namespace TestsUnitairesPourServices.Services.Tests
 {
 
-    [TestClass()]
+    [TestClass]
     public class CatsServiceTests
     {
         const int CHAT_1 = 1;
         const int CHAT_2 = 2;
         const int MAISON_1 = 1;
         const int MAISON_2 = 2;
-        // TODO Mettre seulement les optionms ici et non la BD en entier
-        // La BD doit être crée et détruite pour chacun des tests, sinon il y aura des problèmes avec le tracking des éléments
-        private DbContextOptions<ApplicationDBContext> _options;
-        public CatsServiceTests()
-        {
-            // TODO On initialise les options de la BD, on utilise une InMemoryDatabase
-            _options = new DbContextOptionsBuilder<ApplicationDBContext>()
-                // TODO il faut installer la dépendance Microsoft.EntityFrameworkCore.InMemory
-                .UseInMemoryDatabase(databaseName: "CatsService")
-                .UseLazyLoadingProxies(true) // Active le lazy loading
-                .Options;
-        }
+        
+        private ApplicationDBContext _db;
 
         [TestInitialize]
-    public void Init()
-    {
-        // TODO avoir la durée de vie d'un context la plus petite possible
-        using ApplicationDBContext db = new ApplicationDBContext(_options);
+        public void Init()
+        {
+            // En utilisant un nom différent à chaque fois, on n'a pas besoin de retirer les données
+            string dbName = "CardsService" + Guid.NewGuid().ToString();
+            // TODO On initialise les options de la BD, on utilise une InMemoryDatabase
+            DbContextOptions<ApplicationDBContext> options = new DbContextOptionsBuilder<ApplicationDBContext>()
+                // TODO il faut installer la dépendance Microsoft.EntityFrameworkCore.InMemory
+                .UseInMemoryDatabase(databaseName: dbName)
+                .UseLazyLoadingProxies(true) // Active le lazy loading
+                .Options;
+
+            _db = new ApplicationDBContext(options);
             // TODO on ajoute des données de tests
             House[] Maisons = new House[] {
                 new House
                 {
-                    Id = 1,
+                    Id = MAISON_1,
                     Address = "5555 rue quoi",
                     OwnerName = "Kayla"
                 },
                 new House()
                 {
-                    Id = 2,
+                    Id = MAISON_2,
                     Address = "2341 rue quoi",
                     OwnerName = "Jay"
                 }
@@ -56,33 +58,98 @@ namespace TestsUnitairesPourServices.Services.Tests
             Cat[] Chats = new Cat[] {
                 new Cat
                 {
-                    Id = 1,
+                    Id = CHAT_1,
                     Name = "Todd",
                     Age = 12,
                     House = Maisons[0]
                 },
                 new Cat()
                 {
-                    Id = 2,
+                    Id = CHAT_2,
                     Name = "Billie",
                     Age = 4
                 }
             };
-            db.AddRange(Maisons);
-            db.AddRange(Chats);
-            db.SaveChanges();
-    }
-    [TestCleanup]
-    public void Dispose()
-    {
-        //TODO on efface les données de tests pour remettre la BD dans son état initial
-        using ApplicationDBContext db = new ApplicationDBContext(_options);
-        
-    }
-    [TestMethod()]
-        public void MoveTest()
+            _db.AddRange(Maisons);
+            _db.AddRange(Chats);
+            _db.SaveChanges();
+        }
+
+        [TestCleanup]
+        public void Dispose()
         {
-            Assert.Fail();
+            _db.Dispose();
+
+        }
+        [TestMethod]
+        public void MoveSuccesTest()
+        {
+            //Arrange
+            CatsService _catservice = new CatsService(_db);
+            House? maison1 = _db.House.Find(MAISON_1);
+            House? maison2 = _db.House.Find(MAISON_2);
+            //Act
+            var cat = _catservice.Move(CHAT_1, maison1, maison2);
+            //Assert
+            Assert.AreEqual(cat.House, maison2);
+        }
+
+        [TestMethod]
+        public void Move_Succes_RetournePasNullTest()
+        {
+            //Arrange
+            var _catservice = new CatsService(_db);
+            House? maison1 = _db.House.Find(MAISON_1);
+            House? maison2 = _db.House.Find(MAISON_2);
+            //Act
+            var cat = _catservice.Move(CHAT_1, maison1, maison2);
+            //Assert
+            Assert.AreNotEqual(cat, null);
+        }
+
+        [TestMethod]
+        public void Move_ChatIdInconnue_RetourneNullTest()
+        {
+            //Arrange
+            var _catservice = new CatsService(_db);
+            House? maison1 = _db.House.Find(MAISON_1);
+            House? maison2 = _db.House.Find(MAISON_2);
+            //Act
+            int id = 3;
+            if (_db.Cat.Any(c => c.Id == id)) Assert.Fail(); //Si id existe, le test devient inutile. Il ne retournera pas null.
+            var cat = _catservice.Move(id, maison1, maison2);
+            //Assert
+            Assert.AreEqual(cat.House, null);
+        }
+
+        [TestMethod]
+        public void Move_ChatAucuneMaison_LanceWildCatExceptionTest()
+        {
+            //Arrange
+            var _catservice = new CatsService(_db);
+            House? maison1 = _db.House.Find(MAISON_1);
+            House? maison2 = _db.House.Find(MAISON_2);
+            //Act
+            var cat = _catservice.Move(CHAT_2, maison2, maison1);
+            //Assert
+            var exception = Assert.ThrowsException<WildCatException>(()=> cat);
+            Assert.AreEqual("On n'apprivoise pas les chats sauvages", exception.Message);
+        }
+
+        [TestMethod]
+        public void Move_ChatNeCommencePasAPartirDeSaMaison_LanceDontStealMyCatExceptionTest()
+        {
+            //Arrange
+            var _catservice = new CatsService(_db);
+            House? maison1 = _db.House.Find(MAISON_1);
+            House? maison2 = _db.House.Find(MAISON_2);
+            //Act
+            var cat = _catservice.Move(CHAT_2, maison2, maison1);
+            //Assert
+            var exception = Assert.ThrowsException<DontStealMyCatException>(() => cat);
+            Assert.AreEqual("Touche pas à mon chat!", exception.Message);
         }
     }
-}
+    
+
+}  
